7f3567a04129316979eb3bdbf37189d3d846f2f3
May Lee <mayhclee@gmail.com>
2025-09-15 22:07:26 -0700

    Wave 01 Passed

--- adagrams/game.py
+++ adagrams/game.py
@@ -1,8 +1,52 @@
 from random import randint
 
-def draw_letters():
-    pass
+LETTER_POOL = {
+    'A': 9, 
+    'B': 2, 
+    'C': 2, 
+    'D': 4, 
+    'E': 12, 
+    'F': 2, 
+    'G': 3, 
+    'H': 2, 
+    'I': 9, 
+    'J': 1, 
+    'K': 1, 
+    'L': 4, 
+    'M': 2, 
+    'N': 6, 
+    'O': 8, 
+    'P': 2, 
+    'Q': 1, 
+    'R': 6, 
+    'S': 4, 
+    'T': 6, 
+    'U': 4, 
+    'V': 2, 
+    'W': 2, 
+    'X': 1, 
+    'Y': 2, 
+    'Z': 1
+}
 
+def available_letters():
+    available_letters = []
+    for letter, quantity in LETTER_POOL.items():
+        available_letters.extend(([letter] * quantity))
+    return available_letters
+    
+
+def draw_letters():
+    letters_list = available_letters()
+    letters = []
+    
+    while len(letters) < 10:
+        i = randint(1, len(letters_list)-1)
+        letters.append(letters_list[i])
+        letters_list.pop(i)
+
+    return letters
+    
 def uses_available_letters(word, letter_bank):
     pass
 
@@ -11,3 +55,5 @@ def score_word(word):
 
 def get_highest_word_score(word_list):
     pass
+
+draw_letters()
