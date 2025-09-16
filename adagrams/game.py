a60dc84722f5aac1a5651db61d09f070ca9da3c2
May Lee <mayhclee@gmail.com>
2025-09-16 00:29:34 -0700

    Wave 04 Passed

--- adagrams/game.py
+++ adagrams/game.py
@@ -1,5 +1,6 @@
 from random import randint
 
+#adding the letter pool to game.py for reference
 LETTER_POOL = {
     'A': 9, 
     'B': 2, 
@@ -29,6 +30,7 @@ LETTER_POOL = {
     'Z': 1
 }
 
+#including score counter 
 LETTER_SCORE = {
     1:['A','E','I','O','U','L','N','R','S','T'],
     2:['D','G'],
@@ -39,12 +41,14 @@ LETTER_SCORE = {
     10:['Q','Z']
 }
 
+#function for converting the letter pool in a copied list for use
 def available_letters():
 
     available_letters = []
 
     for letter, quantity in LETTER_POOL.items():
         available_letters.extend(([letter] * quantity))
+
     return available_letters
     
 
@@ -60,12 +64,13 @@ def draw_letters():
 
     return letters
 
+#function to take the word and spliting them into a list without the .split() function
 def making_words_into_list(words):
 
     usable_words = []
 
     for letter in words:
-        usable_words.append(letter.upper())
+        usable_words.append(letter.upper()) #making sure everything is uppercase
     
     return usable_words
 
@@ -73,9 +78,10 @@ def uses_available_letters(word, letter_bank):
 
     split_word = making_words_into_list(word)
 
+    #checking the number of time the letter appears on the bank vs in the actual word
     for letter in split_word:
         if (letter not in letter_bank or 
-            split_word.count(letter)>letter_bank.count(letter)):
+            split_word.count(letter)>letter_bank.count(letter)):  
             is_valid = False
         else:
             is_valid = True
@@ -87,16 +93,41 @@ def score_word(word):
     word_for_scoring = making_words_into_list(word)
     score = 0
 
+    #checking if letter is in the score dictionary and adding it's score
     for letter in word_for_scoring:
-        for points in LETTER_SCORE.keys():
-            if letter in LETTER_SCORE[points]:
+        for points in LETTER_SCORE.keys(): 
+            if letter in LETTER_SCORE[points]: 
                 score += points
     
-    if len(word_for_scoring) > 6:
+    #if word is has 7 letters or more
+    if len(word_for_scoring) >= 7:
         score += 8
 
     return score
 
 def get_highest_word_score(word_list):
-    pass
+    
+    highest_word = ""
+    highest_points = 0
+    
+
+    for word in word_list:
+        
+        points = score_word(word)
+    
+        #getting the score of each word
+        if points > highest_points:
+            highest_points = points
+            highest_word = word
+        elif points == highest_points:
+            if len(word) == len(highest_word):
+                continue
+            elif len(word) == 10:
+                highest_word = word
+                continue
+            elif len(word) < len(highest_word) and len(highest_word) != 10:
+                highest_word = word
+
+    #returning value as a tuple
+    return (highest_word,highest_points)
 
